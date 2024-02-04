4b7bbd81697a03cbeeb75bbb8c9e294beb313650
Magdalena2013 <146621991+Magdalena2013@users.noreply.github.com>
2024-02-04 10:42:20 +0200

    Add files via upload

---
+++ S/script.js
@@ -0,0 +1,52 @@
+// let -- variabila rescriptibila
+// var -- versiunea mai veche "let"
+// const -- variabila constanta
+
+// let name = "Name"; //String
+// let age = 12345; //Number
+// let bool = true; //boolean
+// let nul = null; // fara value
+// let apple; // undefine
+// let n =NaN; // nan value
+// let nameF = (p1, p2) =>
+
+// + - * / % ** // math opperation
+// let value = 12.0;
+
+// // = atribuim
+// // == verificam/comparam daca valorile sunt la fel
+// // === compara si tipul variabilei
+
+// //12 = "12" => True
+// //12 === "12" => False
+// if (value = 12){
+//     console.log("True")
+// }else{
+//     console.log("Wrong")
+// }
+// //v = start; end; pass
+// //                i + 1
+// for (i = 0: i <= 10; i++){
+//     // o => 1 => 2 => 3 => ........ => 10
+// }
+
+// // for(element => element.age); // for each
+// let count = 0;
+// while(count < 10){
+//     console.log(count);
+//     count = count + 1;
+//     // count += 1
+// }
+// function nameFunc(p1, p2, p3){
+//     //code
+// }
+
+// class className{
+//     constructor(p1, p2){
+//         this.p1 = p1;
+//         this.p2 = p2;
+//     }
+//     classFunc() {
+//         console.log(this.p1 + this.p2);
+//     }
+// }
